efecf1c80e93801fd15ee8313bcafebdc2d74512
Aleksey Avetisian <s1t3w4n@yandex.ru>
2020-10-31 11:48:33 +0300

    Initial commit

---
+++ src/test/java/ru/ideaplatform/flights/FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ideaplatform.flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
