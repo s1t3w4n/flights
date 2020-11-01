397c53495a70a9edaaf812abc70625785a160f1e
Aleksey Avetisian <s1t3w4n@yandex.ru>
2020-11-01 16:01:39 +0300

    have fixed problems with packaging

--- src/test/java/ru/ideaplatform/flights/service/FlightServiceImplWithDataTest.java
+++ src/test/java/ru/ideaplatform/flights/service/FlightServiceImplWithDataTest.java
@@ -1,20 +1,16 @@
 package ru.ideaplatform.flights.service;
 
+import com.google.gson.Gson;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.shell.jline.ScriptShellApplicationRunner;
-import ru.ideaplatform.flights.exception.NotJsonFileFormat;
 import ru.ideaplatform.flights.model.Flight;
 
-import java.io.FileNotFoundException;
 import java.math.BigDecimal;
-import java.util.Locale;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,21 +19,23 @@ import static org.assertj.core.api.Assertions.assertThat;
         InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
         ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
 })
-@Import(FlightServiceImpl.class)
 class FlightServiceImplWithDataTest {
 
     private static final String EMPTY_VALUE_PARAMETER = "";
     private static final Flight LOWEST_DEFAULT_FLIGHT = new Flight("Москва", "Хабаровск", 11000);
     private static final Flight HIGHEST_DEFAULT_FLIGHT = new Flight("Москва", "Хабаровск", 12500);
-    public static final BigDecimal DEFAULT_DECIMAL = new BigDecimal("11833.33");
-
-    @Autowired
-    private FlightService service;
-
-    @BeforeEach
-    public void fillData() throws NotJsonFileFormat, FileNotFoundException {
-        service.load(EMPTY_VALUE_PARAMETER);
+    private static final Flight EXTRA_FLIGHT = new Flight("Москва", "Хабаровск", 12000);
+    private static final BigDecimal DEFAULT_DECIMAL = new BigDecimal("11833.33");
+    private static final Map<String, Map<String, LinkedList<Flight>>> data = new HashMap<>();
+    static {
+        data.put("Москва", new HashMap<>());
+        data.get("Москва").put("Хабаровск", new LinkedList<>());
+        data.get("Москва").get("Хабаровск").add(LOWEST_DEFAULT_FLIGHT);
+        data.get("Москва").get("Хабаровск").add(EXTRA_FLIGHT);
+        data.get("Москва").get("Хабаровск").add(HIGHEST_DEFAULT_FLIGHT);
     }
+
+    private final FlightService service = new FlightServiceImpl(new Gson(), data);
 
     @SneakyThrows
     @DisplayName("Should find lowest price with empty parameters")
